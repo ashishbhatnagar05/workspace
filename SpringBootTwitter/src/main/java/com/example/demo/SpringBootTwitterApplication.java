00ea4cc1d741e10b127f0941da43f0c26a04a958
kaal-PC\kaal <ashishbhatnagar05@gmail.com>
2017-09-24 01:41:22 +0530

    first commit

---
+++ SpringBootTwitter/src/main/java/com/example/demo/SpringBootTwitterApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTwitterApplication.class, args);
+	}
+}
